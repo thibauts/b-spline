ed46dcbb8eb55c5663e07ed34a4b04b408df322e
Pomax <pomax@nihongoresources.com>
2016-07-22 11:14:00 -0700

    code legibility improvement on the boosted NURBS test

--- test/index.js
+++ test/index.js
@@ -141,21 +141,8 @@ expected = [
 verify("non-uniform rational b-spline test", tvalues, points, expected, order, knots, weights);
 
 // test non-uniform rational curve with boosted weights
-points = [
-  [ 0.0, -0.5],
-  [-0.5, -0.5],
-  [-0.5,  0.0],
-  [-0.5,  0.5],
-  [ 0.0,  0.5],
-  [ 0.5,  0.5],
-  [ 0.5,  0.0],
-  [ 0.5, -0.5],
-  // duplicated first point
-  [ 0.0, -0.5]
-]
-knots = [0, 0, 0, 1/4, 1/4, 1/2, 1/2, 3/4, 3/4, 1, 1, 1];
-var w = 4 * Math.pow(0.5, 0.5);
-var weights = [1, w, 1, w, 1, w, 1, w, 1];
+var boosted = 4 * w;
+var weights = [1, boosted, 1, boosted, 1, boosted, 1, boosted, 1];
 expected = [
   [  0     , -0.5    ],
   [ -0.4041, -0.4574 ],
